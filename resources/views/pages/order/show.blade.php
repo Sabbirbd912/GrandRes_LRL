08967a8fa3022bf069ff94f66a90bce593dc861e
Sabbir Ahmed <sabbirbd912@gmail.com>
2025-06-21 00:36:55 +0600

    Updated invoice view and image

--- resources/views/pages/order/show.blade.php
+++ resources/views/pages/order/show.blade.php
@@ -95,6 +95,40 @@
             </table>
         </div>
         
+        <div class="row justify-content-end">
+            <div class="col-md-4">
+                <table class="table table-borderless text-end">
+                    <tr>
+                        <th class="text-muted">Subtotal:</th>
+                        
+                        <td id="subTotal" class="fw-bold">{{ $sub_total= number_format($detail->qty * $detail->price, 2) }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-muted">Discount (5%):</th>
+                        <td id="discount" class="fw-bold">{{ $dis=$sub_total*0.95, 2 }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-muted">Tax (3%):</th>
+                        <td id="tax" class="fw-bold">{{ $tax= $dis*1.03, 2 }}</td>
+                    </tr>
+                    <tr class="border-top">
+                        <th class="text-dark">Total:</th>
+                        <td id="netTotal" class="fw-bold text-dark">{{$tax}}</td>
+                    </tr>
+                    <tr class="border-top fw-bold">
+                        <th class="text-danger">Amount Due:</th>
+                        <td id="due" class="text-danger">$00.00</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="button" class="btn btn-secondary w-100 mt-2" onclick="CreatePayment()" value="🧾 Create Payments" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+
     </div>
     <div class="card-footer bg-light">
         <p class="fs--1 mb-0"><strong>Notes: </strong>We really appreciate your business and if there’s anything
@@ -104,93 +138,6 @@
 
 <script>
    let base_url="http://localhost/intellect8/api";
-   let cart=[];
-
-
-   document.querySelector("#btnAdd").addEventListener("click",(e)=>{
-       
-       // let desc=document.querySelector("#description").value;
-        let unit=document.querySelector("#unit").value;
-        let price=document.querySelector("#price").value;
-        let product_id=document.querySelector("#product_id").value;
-        let product_name= document.querySelector("#product_id").options[document.querySelector("#product_id").selectedIndex].text;
-        let vat=0;
-        let discount=0;
-        let lineTotal=unit*price-discount+vat;
-        let json={id:cart.length+1,desc:product_name,product_id:product_id,qty:unit,price:price,discount:discount,vat:vat,lineTotal:lineTotal};
-
-        cart.push(json);        
-        console.log(cart);
-        printCart();
-    });
-
-
-    function printCart(){
-        let html="";
-        let total=0;
-        cart.forEach((item)=>{
-            html+="<tr>";
-            html+=`<td class="align-middle"><h6 class="mb-0 text-nowrap">${item.desc}</h6><p class="mb-0">${item.id}</p></td>`;
-            html+=`<td class="align-middle text-center">${item.qty}</td>`;
-            html+=`<td class="align-middle text-end">${item.price}</td>`;
-            html+=`<td class="align-middle text-end">${item.lineTotal}</td>`;
-            html+=`<td><input type="button" onclick="del(${item.id})" value="del" /></td>`;
-            html+="</tr>";
-            total+=item.lineTotal;
-        });
-
-      document.querySelector("#tbody").innerHTML=html;
-      document.querySelector("#subTotal").innerHTML=total;
-      document.querySelector("#netTotal").innerHTML=total;
-
-    }
-
-
-    function del(id){
-      let index=cart.findIndex((item)=>{
-        return item.id==id;
-      });
-       cart.splice(index,1);
-       printCart();
-    }
-
-    async function CreateInvoice(){        
-
-      if(confirm("Are you sure?")){
-        let date=document.querySelector("#date").innerHTML;
-        let customer_id=document.querySelector("#customer_id").value;
-        let total=document.querySelector("#netTotal").innerHTML;
-        
-        
-        let jsonData={
-            created_at:date,
-            updated_at:date,
-            customer_id:customer_id,
-            remark:"Na",
-            payment_term:"CASH",
-            invoice_total:total,
-            paid_total:total,
-            previous_due:0,
-            items:cart
-        }
-
-        console.log(jsonData);
-
-        let response=await fetch(`${base_url}/invoice/save`,{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(jsonData)
-      });
-
-      let json=response.json();
-      console.log(json);
-
-      cart=[];
-      printCart();
-
-      }
-
-}
 
 </script>
 
